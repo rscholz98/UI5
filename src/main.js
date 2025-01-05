016dbb9ec2e11f71d49a491c326fd7128cc27b7d
rscholz98 <richardscholz1@gmx.de>
2025-01-05 16:39:56 +0100

    initial commit: add project structure with main files, styles, and counter functionality

---
+++ src/main.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import "./style.css";
+import "@ui5/webcomponents/dist/Button.js";
+
+document.querySelector("#app").innerHTML = `
+  <ui5-button>Hello UI5 Web Components</ui5-button>
+`;
